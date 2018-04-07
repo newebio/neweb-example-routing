074665778f473a82410b759da6458829affc1861
Vitaly Arkhipov <arvitaly@list.ru>
2018-04-07 14:40:52 +0300

    added statuses

--- app/frames/parent/view.js
+++ app/frames/parent/view.js
@@ -1,9 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const neweb_1 = require("neweb");
 const React = require("react");
 class default_1 extends React.Component {
     render() {
         return React.createElement("div", null,
+            React.createElement("div", null,
+                React.createElement(neweb_1.SeansStatusContext.Consumer, null, (status) => {
+                    return React.createElement("div", null,
+                        "Status:",
+                        status ? status : "");
+                }),
+                React.createElement(neweb_1.NetworkStatusContext.Consumer, null, (status) => {
+                    return React.createElement("div", null,
+                        "Network status:",
+                        status ? status : "");
+                })),
             React.createElement("h5", null,
                 "Layout ",
                 this.props.params ? this.props.params.test : null,
--- app/frames/parent/view.tsx
+++ app/frames/parent/view.tsx
@@ -1,9 +1,21 @@
-import { IViewProps } from "neweb";
+import { IViewProps, NetworkStatusContext, SeansStatusContext } from "neweb";
 import React = require("react");
 
 export default class extends React.Component<IViewProps<any, any>, {}> {
     public render() {
-        return <div><h5>Layout {this.props.params ? this.props.params.test : null} {this.props.data}</h5>
+        return <div>
+            <div><SeansStatusContext.Consumer>
+                {(status: string) => {
+                    return <div>Status:{status ? status : ""}</div>;
+                }}
+            </SeansStatusContext.Consumer>
+                <NetworkStatusContext.Consumer>
+                    {(status: string) => {
+                        return <div>Network status:{status ? status : ""}</div>;
+                    }}
+                </NetworkStatusContext.Consumer>
+            </div>
+            <h5>Layout {this.props.params ? this.props.params.test : null} {this.props.data}</h5>
             {this.props.children}
         </div>;
     }
